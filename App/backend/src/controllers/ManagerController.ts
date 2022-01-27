21723a6b163a76a75a8d3179ae23ab3cd6b2ef4e
mikaellafs <mikaellaferreira0@gmail.com>
2022-01-27 07:15:21 -0300

    Correção dos controllers de vacina e gestor

--- App/backend/src/controllers/ManagerController.ts
+++ App/backend/src/controllers/ManagerController.ts
@@ -1,22 +1,34 @@
-import localizacao from '../../../../Database/data_for_app/Gestores/datasSimulation.json'
-import gestoresInfo from '../../../../Database/data_for_app/Gestores/gestores.json'
+import { Manager, ManagerLocation } from '../models/Manager'
+
+const fs = require('fs');
+
+const pathToLocations = __dirname.replace("App/backend/src/controllers","Database/data_for_app/Gestores/datasSimulation.json");
+const pathToGestoresInfo = __dirname.replace("App/backend/src/controllers", "Database/data_for_app/Gestores/gestores.json");
 
 class ManagerController {
-      getManagers() {
-        return gestoresInfo.gestores
-      }
+    getManagers() {
+        let data = fs.readFileSync(pathToGestoresInfo)
+        let gestores : [Manager] = JSON.parse(data).gestores
+        return gestores;
+    }
 
-      getManager(id: number) {
-        return gestoresInfo.gestores.find(gestor => gestor.id == id)
-      }
+    getManager(id: number) {
+        let data = fs.readFileSync(pathToGestoresInfo)
+        let gestores : [Manager] = JSON.parse(data).gestores
+        return gestores.find(gestor => gestor.id == id);
+    }
 
-      getManagersLocation() {
-        return localizacao
-      }
+    getManagersLocation() {
+        let data = fs.readFileSync(pathToLocations)
+        let locations : [ManagerLocation] = JSON.parse(data)
+        return locations;
+    }
 
-      getManagerLocation(id: number) {
-        return localizacao.find(gestor => gestor.id == id)
-      }
+    getManagerLocation(id: number) {
+        let data = fs.readFileSync(pathToLocations)
+        let locations : [ManagerLocation] = JSON.parse(data)
+        return locations.find(gestor => gestor.id == id);
+    }
 }
 
 export = new ManagerController();
